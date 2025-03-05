dc24da1b6e4302e6e638aff7e4b06f43f3730249
matthewbotpress <matthew.petruzziello@botpress.com>
2025-03-05 12:10:57 -0500

    updating dependencies

--- integrations/zoho-sales-iq-hitl/integration.definition.ts
+++ integrations/zoho-sales-iq-hitl/integration.definition.ts
@@ -6,7 +6,7 @@ import { events, configuration, channels, states, user } from './src/definitions
 export default new IntegrationDefinition({
   name: INTEGRATION_NAME,
   title: 'Zoho Sales IQ HITL',
-  version: '1.0.0',
+  version: '1.0.2',
   icon: 'icon.svg',
   description: 'This integration allows your bot to use Zoho Sales IQ as a HITL Provider',
   readme: 'hub.md',
