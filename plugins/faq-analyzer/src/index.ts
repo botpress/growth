42fe2b690c8f935990c81b546d2aaea3805d5536
matthewbotpress <matthew.petruzziello@botpress.com>
2025-05-02 15:36:17 -0400

    fix(plugins): table capabilities

--- plugins/faq-analyzer/src/index.ts
+++ plugins/faq-analyzer/src/index.ts
@@ -1,5 +1,8 @@
 import * as bp from '.botpress'
 import * as client from '@botpress/client'
+import * as error from './error'
+import * as vanilla from './vanilla-client'
+import * as table from './table'
 
 const plugin = new bp.Plugin({
   actions: {},
