22a65f6f208d2d049a3a2ba6b1385e6a2166ec2b
matthewbotpress <matthew.petruzziello@botpress.com>
2025-05-13 14:53:06 -0400

    fix: icon

--- plugins/faq-analyzer/plugin.definition.ts
+++ plugins/faq-analyzer/plugin.definition.ts
@@ -3,7 +3,7 @@ import * as sdk from "@botpress/sdk";
 
 export default new PluginDefinition({
   name: "plus/faq-analyzer",
-  version: "1.4.1",
+  version: "1.4.2",
   configuration: {
     schema: sdk.z.object({
       tableName: sdk.z
