efba5f88fa897255e6f0b07ba83d14b09cb21b17
matthewbotpress <matthew.petruzziello@botpress.com>
2025-05-21 13:19:23 -0400

    chore(plugin/faq-analyzer): version bump

--- plugins/faq-analyzer/plugin.definition.ts
+++ plugins/faq-analyzer/plugin.definition.ts
@@ -3,7 +3,7 @@ import * as sdk from "@botpress/sdk";
 
 export default new PluginDefinition({
   name: "plus/faq-analyzer",
-  version: "1.5.0",
+  version: "1.5.1",
   icon: 'icon.svg',
   readme: 'hub.md',
   configuration: {
