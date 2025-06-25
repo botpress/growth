2c4b7303eb665d3fe10eab14e10f2fddd1461cf5
Pascal Breton <pascal.breton@botpress.com>
2025-06-25 13:22:29 -0400

    chore(hitl-salesforce): update for hitl v2

--- integrations/hitl-salesforce/bp_modules/hitl/definition/entities/hitlSession.ts
+++ integrations/hitl-salesforce/bp_modules/hitl/definition/entities/hitlSession.ts
@@ -3,6 +3,9 @@
 // This file is generated. Do not edit it manually.
 
 import { z } from "@botpress/sdk";
-export const output = {
+export const hitlSession = {
+  title: "HITL session",
+  description:
+    "A HITL session, often referred to as a ticket or conversation in external systems",
   schema: z.object({}).catchall(z.never()),
 };
