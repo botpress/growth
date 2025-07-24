58efa34d98a4ce6353c9f001b8b344ca75e1d6a6
matthewbotpress <matthew.petruzziello@botpress.com>
2025-07-24 00:37:57 -0400

    chore: title update

--- integrations/google-sheets/integration.definition.ts
+++ integrations/google-sheets/integration.definition.ts
@@ -5,6 +5,7 @@ import { configuration, actions, events, states } from './src/definitions'
 export default new IntegrationDefinition({
   name: integrationName,
   version: '0.0.2',
+  title: 'Google Sheets Public Sync',
   readme: 'hub.md',
   icon: 'icon.svg',
   configuration,
