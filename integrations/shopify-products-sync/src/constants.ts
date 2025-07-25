d515a10efb56c3a0ddf49909006f67fb5c45261d
Audric Pandelé <audricpandele@Audrics-MacBook-Pro.local>
2025-07-25 16:46:38 -0400

    feature: add table sync to shopify integration

--- integrations/shopify-products-sync/src/constants.ts
+++ integrations/shopify-products-sync/src/constants.ts
@@ -1,4 +1,5 @@
 export const SHOPIFY_API_VERSION = '2024-07';
+export const PRODUCTS_TABLE_NAME = 'shopify_products_Table';
 export const SHOPIFY_WEBHOOK_TOPICS = [
   'products/create',
   'products/update',
--- integrations/shopify-products-sync/src/schemas/products.ts
+++ integrations/shopify-products-sync/src/schemas/products.ts
@@ -37,3 +37,24 @@ export type StoredProduct = {
   options?: string[]; // e.g., sizes, colors
   url: string;
 }
+
+export const PRODUCT_TABLE_SCHEMA = {
+  type: 'object',
+  properties: {
+    product_id: { type: 'number', 'x-zui': { searchable: false }  },
+    name: { type: 'string', 'x-zui': { searchable: false }  },
+    sku: { type: 'string', 'x-zui': { searchable: false }  },
+    price: { type: 'number', 'x-zui': { searchable: false }  },
+    weight: { type: 'number', 'x-zui': { searchable: false }  },
+    type: { type: 'string', 'x-zui': { searchable: false }  }, // Shopify's product_type
+    brand_name: { type: 'string', 'x-zui': { searchable: false }  }, // Shopify's vendor
+    categories: { type: 'string', 'x-zui': { searchable: false }  }, // Shopify's tags
+    availability: { type: 'string', 'x-zui': { searchable: false }  }, // Shopify's status
+    is_visible: { type: 'boolean', 'x-zui': { searchable: false }  },
+    description: { type: 'string', 'x-zui': { searchable: false }  },
+    image_url: { type: 'string', 'x-zui': { searchable: false }  },
+    url: { type: 'string', 'x-zui': { searchable: false }  },
+    aggregate: { type: 'string', 'x-zui': { searchable: true }  },
+  },
+  required: ['product_id', 'aggregate'],
+}
