7cef37bb396536d3cb94308bcc3b91d012eb138d
Matthew Petruzziello <matthew.petruzziello@botpress.com>
2025-08-25 13:46:14 -0400

    fix(integrations/livechat-hitl): old bp modules causing incompatibility bug (#152)

--- integrations/livechat-hitl/bp_modules/hitl/definition/entities/index.ts
+++ integrations/livechat-hitl/bp_modules/hitl/definition/entities/index.ts
@@ -1,5 +1,9 @@
 /* eslint-disable */
 /* tslint:disable */
 // This file is generated. Do not edit it manually.
+import * as hitlSession from "./hitlSession";
+export * as hitlSession from "./hitlSession";
 
-export const entities = {};
+export const entities = {
+  "hitlSession": hitlSession.hitlSession,
+}
