97bd1c95852c434763c709048a2c795f2ebe2668
Simon Risk <simonnrisk@gmail.com>
2025-09-16 12:12:38 -0400

    chore: removed unused type

--- integrations/klaviyo/src/actions/types.ts
+++ integrations/klaviyo/src/actions/types.ts
@@ -1,47 +1,32 @@
-export type KlaviyoError = {
-  id: string
-  code: string
-  title: string
-  detail: string
-  source: {
-    pointer: string
-    parameter: string
-  }
-}
-
-export type KlaviyoApiError = {
-  errors: KlaviyoError[]
-}
-
 export type ProfileSubscriptions = {
   email?: {
     marketing: {
-      consent: 'SUBSCRIBED'
-      consented_at?: string
-    }
-  }
+      consent: "SUBSCRIBED";
+      consented_at?: string;
+    };
+  };
   sms?: {
     marketing: {
-      consent: 'SUBSCRIBED'
-      consented_at?: string
-    }
-  }
-}
+      consent: "SUBSCRIBED";
+      consented_at?: string;
+    };
+  };
+};
 
 export type GetProfilesOptions = {
-  filter?: string
-  pageSize?: number
+  filter?: string;
+  pageSize?: number;
   sort?:
-    | 'created'
-    | '-created'
-    | 'email'
-    | '-email'
-    | 'id'
-    | '-id'
-    | 'subscriptions.email.marketing.list_suppressions.timestamp'
-    | '-subscriptions.email.marketing.list_suppressions.timestamp'
-    | 'subscriptions.email.marketing.suppression.timestamp'
-    | '-subscriptions.email.marketing.suppression.timestamp'
-    | 'updated'
-    | '-updated'
-}
+    | "created"
+    | "-created"
+    | "email"
+    | "-email"
+    | "id"
+    | "-id"
+    | "subscriptions.email.marketing.list_suppressions.timestamp"
+    | "-subscriptions.email.marketing.list_suppressions.timestamp"
+    | "subscriptions.email.marketing.suppression.timestamp"
+    | "-subscriptions.email.marketing.suppression.timestamp"
+    | "updated"
+    | "-updated";
+};
