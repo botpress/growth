0e5c70c2bf2d906f6db84c353349f347fcffd3d3
ermek-botpress <ermek.barmashev@botpress.com>
2025-09-17 16:25:18 -0400

    chore(klaviyo): bump up the version and remove plus handle (#164)

--- integrations/klaviyo/integration.definition.ts
+++ integrations/klaviyo/integration.definition.ts
@@ -2,11 +2,11 @@ import { IntegrationDefinition, z } from "@botpress/sdk";
 import { actions } from "./definitions";
 
 export default new IntegrationDefinition({
-  name: "plus/klaviyo",
+  name: "klaviyo",
   title: "Klaviyo",
   description:
     "Manage customer profiles, generate leads, and curate marketing campaigns",
-  version: "1.0.0",
+  version: "3.0.0",
   readme: "hub.md",
   icon: "icon.svg",
   configuration: {
