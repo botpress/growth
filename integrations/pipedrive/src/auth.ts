8c4a61f2d6afec7dd455ee8f7adfe6f3fa4c5cc1
Ceyhun Topcu <ceyhuntopcu@Mac.localdomain>
2025-09-17 15:40:22 -0400

    better way to ensure auth

--- integrations/pipedrive/src/auth.ts
+++ integrations/pipedrive/src/auth.ts
@@ -1,6 +1,6 @@
 import { RuntimeError } from '@botpress/sdk'
 import * as bp from '.botpress'
-import { v2 } from 'pipedrive'
+import { v2, v1 } from 'pipedrive'
 
 export const getApiConfig = async ({ ctx }: { ctx: bp.Context }): Promise<v2.Configuration> => {
   const apiKey = ctx.configuration.apiKey
@@ -9,9 +9,9 @@ export const getApiConfig = async ({ ctx }: { ctx: bp.Context }): Promise<v2.Con
 
 export const validateCredentials = async ({ ctx }: { ctx: bp.Context }) => {
   try {
-    const api = new v2.PersonsApi(await getApiConfig({ ctx }))
-    await api.getPersons({ limit: 1 })
-  } catch (e) {
+    const api = new v1.UsersApi(new v1.Configuration({ apiKey: ctx.configuration.apiKey }))
+    await api.getCurrentUser()
+  } catch {
     throw new RuntimeError('Invalid Pipedrive API key')
   }
 }
