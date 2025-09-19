07d8fe5df4325da1f413820209cee55fb317a1b0
ceyhun-botpress <ceyhun.topcu@botpress.com>
2025-09-19 10:32:52 -0400

    bumped up major version (#173)
    Co-authored-by: Ceyhun Topcu <ceyhuntopcu@Ceyhuns-MacBook-Pro.local>

--- integrations/pipedrive/integration.definition.ts
+++ integrations/pipedrive/integration.definition.ts
@@ -5,7 +5,7 @@ export default new IntegrationDefinition({
   name: 'pipedrive',
   title: 'Pipedrive',
   description: 'Manage contacts, deals and more from your chatbot.',
-  version: '4.1.0',
+  version: '5.0.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   configuration: {
