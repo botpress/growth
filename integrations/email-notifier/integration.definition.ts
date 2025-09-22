a0a8b7e3638c16924e7483722465a7325860c15e
nabilbotpress <nabil.muzafar@botpress.com>
2025-09-22 09:56:41 -0400

    chore(email-notifier): integration definition attributes

--- integrations/email-notifier/integration.definition.ts
+++ integrations/email-notifier/integration.definition.ts
@@ -5,6 +5,8 @@ import { actions, events, states } from "src/definitions/index";
 export default new IntegrationDefinition({
   name: integrationName,
   version: "1.0.0",
+  title: "Email Notifier",
+  description: "Send emails to your Botpress bot in minutes",
   readme: "hub.md",
   icon: "icon.svg",
   secrets: {
