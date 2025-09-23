6cb7719eb0577289328180ab6bc1c686a28f0f05
Maanav Rajesh <maanav.rajesh@botpress.com>
2025-09-23 17:45:33 -0400

    Update registration

--- integrations/apollo/src/setup.ts
+++ integrations/apollo/src/setup.ts
@@ -1,9 +1,17 @@
 import * as bp from '.botpress'
+import { getApolloClient } from './client'
+import { RuntimeError } from '@botpress/client'
 
-export const register: bp.IntegrationProps['register'] = async () => {
-  console.log('Registering Apollo.io integration')
+export const register: bp.IntegrationProps['register'] = async ({ ctx, logger }) => {
+  try {
+    const apolloClient = getApolloClient(ctx.configuration)
+    await apolloClient.searchContact({ q_keywords: '', page: 1, per_page: 1 })
+    logger.forBot().info('Apollo integration registered successfully')
+  } catch (error) {
+    logger.forBot().error('Failed to register Apollo integration', error)
+    throw new RuntimeError('Failed to register Apollo integration')
+  }
 }
-
 export const unregister: bp.IntegrationProps['unregister'] = async () => {
   console.log('Unregistering Apollo.io integration')
 }
