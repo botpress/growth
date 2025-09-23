52de6dab49b8d0bda470322288f12c4f0c6f05ea
AudricPnd <audric.pandele@botpress.com>
2025-09-23 14:24:34 -0400

    chore: bump major version

--- integrations/magento2/integration.definition.ts
+++ integrations/magento2/integration.definition.ts
@@ -3,7 +3,7 @@ import { integrationName } from './package.json'
 
 export default new IntegrationDefinition({
   name: integrationName,
-  version: '1.0.7',
+  version: '2.0.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   configuration: {
