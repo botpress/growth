651c7856bca0b9d7f2c0b8e2b20481c060ac903e
Maanav Rajesh <maanav.rajesh@botpress.com>
2025-09-25 17:20:04 -0400

    Update version

--- integrations/apollo/integration.definition.ts
+++ integrations/apollo/integration.definition.ts
@@ -6,7 +6,7 @@ import { configuration } from './src/definitions/index'
 export default new IntegrationDefinition({
   name: integrationName,
   title: 'Apollo.io',
-  version: '1.0.0',
+  version: '3.0.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   description: "Search for and enrich people from your team's Apollo.io account",
