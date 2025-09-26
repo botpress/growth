0f5895919747c4a2277b8e73f43aad9f1b640c97
Haris Mahmood <78054284+CodePioneerX@users.noreply.github.com>
2025-09-26 13:11:38 -0400

    [Update] SharePoint Version Num Bump (#182)
    * Update integration.definition.ts updated version number
    
    * Update integration.definition.ts
    
    * Update integration.definition.ts

--- integrations/sharepoint/integration.definition.ts
+++ integrations/sharepoint/integration.definition.ts
@@ -2,7 +2,7 @@ import { z, IntegrationDefinition } from '@botpress/sdk'
 
 export default new IntegrationDefinition({
   name: 'plus/sharepoint',
-  version: '3.0.5',
+  version: '3.1.1',
   title: 'SharePoint',
   description: 'Sync one or many SharePoint document libraries with one or more Botpress knowledge bases.',
   readme: 'hub.md',
