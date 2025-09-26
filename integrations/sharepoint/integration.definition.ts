19f3b349021616b3a34557c17035daee19ba3ecc
CodePioneerX <haris.mahmood.01@hotmail.com>
2025-09-26 09:30:02 -0400

    feat(sharepoint): update integration definition

--- integrations/sharepoint/integration.definition.ts
+++ integrations/sharepoint/integration.definition.ts
@@ -2,7 +2,7 @@ import { z, IntegrationDefinition } from '@botpress/sdk'
 import { integrationName } from './package.json'
 
 export default new IntegrationDefinition({
-  name: integrationName,
+  name: 'plus/sharepoint',
   version: '3.0.5',
   title: 'SharePoint',
   description: 'Sync one or many SharePoint document libraries with one or more Botpress knowledge bases.',
