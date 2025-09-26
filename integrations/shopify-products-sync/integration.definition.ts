99456e23de8995f6a13cb79916588b421efba70e
AudricPnd <audric.pandele@botpress.com>
2025-09-26 09:06:33 -0400

    fix: align versions with deployed

--- integrations/shopify-products-sync/integration.definition.ts
+++ integrations/shopify-products-sync/integration.definition.ts
@@ -4,7 +4,7 @@ import { configuration, states, actions, events } from './src/definitions/index'
 export default new IntegrationDefinition({
   name: 'plus/shopify-products-sync',
   title: 'Shopify products sync',
-  version: '2.0.1',
+  version: '3.0.1',
   readme: 'hub.md',
   icon: 'icon.svg',
   description: 'Sync products from Shopify to Botpress Knowledge Base',
