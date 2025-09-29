44028bce3e67e6c60a49985da342baaf0fef2705
AudricPnd <audric.pandele@botpress.com>
2025-09-29 16:00:46 -0400

    chore: bump apollo version

--- integrations/apollo/integration.definition.ts
+++ integrations/apollo/integration.definition.ts
@@ -5,7 +5,7 @@ import { configuration } from './src/definitions/index'
 export default new IntegrationDefinition({
   name: 'apollo',
   title: 'Apollo.io',
-  version: '3.0.1',
+  version: '3.0.2',
   readme: 'hub.md',
   icon: 'icon.svg',
   description: "Search for and enrich people from your team's Apollo.io account",
