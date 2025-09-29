a086e88ba21ba57b4bb31ce639a3f9ebf130052b
Audric Pandelé <audric.pandele@botpress.com>
2025-09-29 16:11:20 -0400

    bugfix: deploy integrations action login issues (#185)

--- integrations/apollo/integration.definition.ts
+++ integrations/apollo/integration.definition.ts
@@ -1,12 +1,11 @@
 import { IntegrationDefinition } from '@botpress/sdk'
-import { integrationName } from './package.json'
 import { actions } from './src/definitions/actions'
 import { configuration } from './src/definitions/index'
 
 export default new IntegrationDefinition({
-  name: integrationName,
+  name: 'apollo',
   title: 'Apollo.io',
-  version: '3.0.0',
+  version: '3.0.2',
   readme: 'hub.md',
   icon: 'icon.svg',
   description: "Search for and enrich people from your team's Apollo.io account",
