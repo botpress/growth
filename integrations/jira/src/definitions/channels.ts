9e4cde9b931986f623c821ed2bb6633ea7e6f3f5
Simon Risk <simonnrisk@gmail.com>
2025-11-19 16:25:36 -0500

    rev: remove channels files with no functionality

--- integrations/jira/src/definitions/channels.ts
+++
@@ -1,9 +0,0 @@
-import { IntegrationDefinitionProps, messages } from '@botpress/sdk'
-
-export const channels = {
-  channel: {
-    title: 'Channel',
-    description: 'Jira integration channel for receiving messages',
-    messages: { ...messages.defaults },
-  },
-} satisfies IntegrationDefinitionProps['channels']
