17ba8639be349e05696e89784903116a81de1a27
Simon Risk <simonnrisk@gmail.com>
2025-11-19 15:52:10 -0500

    chore: cleanup linting issues

--- integrations/jira/src/definitions/channels.ts
+++ integrations/jira/src/definitions/channels.ts
@@ -2,6 +2,8 @@ import { IntegrationDefinitionProps, messages } from '@botpress/sdk'
 
 export const channels = {
   channel: {
+    title: 'Channel',
+    description: 'Jira integration channel for receiving messages',
     messages: { ...messages.defaults },
   },
 } satisfies IntegrationDefinitionProps['channels']
