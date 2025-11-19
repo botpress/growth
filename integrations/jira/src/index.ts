e38ee4eacac79e24a9f5a6cad54af132585f68a3
Simon Risk <simonnrisk@gmail.com>
2025-11-19 16:25:36 -0500

    rev: remove channels files with no functionality

--- integrations/jira/src/index.ts
+++ integrations/jira/src/index.ts
@@ -1,13 +1,11 @@
 import actions from './actions'
-import { register, unregister, channels, handler } from './setup'
+import { register, unregister } from './setup'
 import * as bp from '.botpress'
-
-console.info('starting integration')
 
 export default new bp.Integration({
   register,
   unregister,
   actions,
-  channels,
-  handler,
+  channels: {},
+  handler: async () => {},
 })
