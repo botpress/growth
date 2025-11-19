e70fc178ced6bfd0df6298c171b1b862e9d2df79
Simon Risk <simonnrisk@gmail.com>
2025-11-19 16:31:06 -0500

    ref: register and unregister into a single setup file

--- integrations/jira/src/setup/unregister.ts
+++
@@ -1,8 +0,0 @@
-import * as bp from '.botpress'
-
-export const unregister: bp.IntegrationProps['unregister'] = async () => {
-  /**
-   * This is called when a bot removes the integration.
-   * You should use this handler to instanciate ressources in the external service and ensure that the configuration is valid.
-   */
-}
