2b84d7a78bbca60134d90f3ee8397e3d580bb893
Simon Risk <simonnrisk@gmail.com>
2025-11-19 13:00:59 -0500

    chore: remove unused types for misc/types

--- integrations/jira/src/misc/types.ts
+++ integrations/jira/src/misc/types.ts
@@ -1,7 +1,4 @@
-import type { IntegrationContext } from '@botpress/sdk'
 import type * as botpress from '.botpress'
-import type { Configuration } from '.botpress/implementation/configuration'
 
 export type Config = botpress.configuration.Configuration
 export type Implementation = ConstructorParameters<typeof botpress.Integration>[0]
-export type IntegrationCtx = IntegrationContext<Configuration>
