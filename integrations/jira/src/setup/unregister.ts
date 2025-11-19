8c27e785e5f39e2b592890b03a8fc617c8948176
Simon Risk <simonnrisk@gmail.com>
2025-11-19 11:35:53 -0500

    fix: bp integration types fpr register and unregister

--- integrations/jira/src/setup/unregister.ts
+++ integrations/jira/src/setup/unregister.ts
@@ -1,6 +1,6 @@
-import type { UnregisterFunction } from '../misc/types'
+import * as bp from '.botpress'
 
-export const unregister: UnregisterFunction = async () => {
+export const unregister: bp.IntegrationProps['unregister'] = async () => {
   /**
    * This is called when a bot removes the integration.
    * You should use this handler to instanciate ressources in the external service and ensure that the configuration is valid.
