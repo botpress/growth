31520c5ee7f3eccb36ab02f9329062736873bd66
Simon Risk <simonnrisk@gmail.com>
2025-11-19 13:21:13 -0500

    ref: remove types.ts file

--- integrations/jira/src/misc/types.ts
+++
@@ -1,4 +0,0 @@
-import type * as botpress from '.botpress'
-
-export type Config = botpress.configuration.Configuration
-export type Implementation = ConstructorParameters<typeof botpress.Integration>[0]
