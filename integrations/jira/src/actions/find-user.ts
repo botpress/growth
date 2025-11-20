a5ce9dd05978ef0a6b8bda0bc2dfb83d69d1b4e5
Simon Risk <simonnrisk@gmail.com>
2025-11-20 12:15:55 -0500

    fix: error handling

--- integrations/jira/src/actions/find-user.ts
+++ integrations/jira/src/actions/find-user.ts
@@ -1,19 +1,24 @@
 import * as bp from '.botpress'
+import { RuntimeError } from '@botpress/sdk'
 import { findUserInputSchema, findUserOutputSchema } from '../misc/custom-schemas'
 import { getClient } from '../utils'
+import { getErrorMessage } from '../utils/error-handler'
 
 export const findUser: bp.IntegrationProps['actions']['findUser'] = async ({ ctx, input, logger }) => {
-  const validatedInput = findUserInputSchema.parse(input)
-  const jiraClient = getClient(ctx.configuration)
-  let response
   try {
-    response = await jiraClient.findUser(validatedInput.accountId)
+    const validatedInput = findUserInputSchema.parse(input)
+    const jiraClient = getClient(ctx.configuration)
+    const response = await jiraClient.findUser(validatedInput.accountId)
     logger
       .forBot()
       .info(`Successful - Find User - ${response?.displayName || 'Unknown'} - with ID: ${validatedInput.accountId}`)
+    return findUserOutputSchema.parse(response)
   } catch (error) {
-    logger.forBot().debug(`'Find User' exception ${JSON.stringify(error)}`)
-    response = {}
+    const errorMessage = getErrorMessage(error)
+    logger.forBot().error(`'Find User' exception: ${errorMessage}`, error)
+    if (error instanceof RuntimeError) {
+      throw error
+    }
+    throw new RuntimeError(`Failed to find Jira user: ${errorMessage}`)
   }
-  return findUserOutputSchema.parse(response)
 }
