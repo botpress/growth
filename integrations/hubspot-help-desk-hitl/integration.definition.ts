1cc2654c9f66da21c2e9fd643e7dbceceeb0c536
sham-bolic <maxcyfong@gmail.com>
2025-11-25 16:29:35 -0500

    veersion bump

--- integrations/hubspot-help-desk-hitl/integration.definition.ts
+++ integrations/hubspot-help-desk-hitl/integration.definition.ts
@@ -5,7 +5,7 @@ import { events, configuration, states, channels, user } from './src/definitions
 export default new IntegrationDefinition({
   name: 'plus/hubspot-help-desk-hitl',
   title: 'HubSpot Help Desk HITL',
-  version: '3.0.1',
+  version: '3.1.0',
   icon: 'icon.svg',
   description:
     'This integration allows your bot to use HubSpot as a HITL provider. Messages will appear in HubSpot Help Desk.',
