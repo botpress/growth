87b6cb90f98fc77fd0a2095c53319d168f44e36b
Ceyhun <ceyhun.topcu@botpress.com>
2025-12-16 17:10:07 -0500

    chore: revert everything, push bp_modules

---
+++ integrations/chatwoot/bp_modules/hitl/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+/* tslint:disable */
+// This file is generated. Do not edit it manually.
+
+import * as sdk from "@botpress/sdk"
+
+import definition from "./definition"
+
+export default {
+  type: "integration",
+  id: "intver_01K0SV5HZAT9FSVVEB7S91SH4F",
+  uri: undefined,
+  name: "hitl",
+  version: "2.0.2",
+  definition,
+} satisfies sdk.IntegrationPackage
