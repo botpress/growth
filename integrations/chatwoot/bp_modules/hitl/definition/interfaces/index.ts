fdf138542d13f4dd5a0a4851bd636ebcc201460f
Ceyhun <ceyhun.topcu@botpress.com>
2025-12-16 11:52:28 -0500

    chore: remove tracked files

--- integrations/chatwoot/bp_modules/hitl/definition/interfaces/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/* tslint:disable */
-// This file is generated. Do not edit it manually.
-import * as hitlHitlSession from "./hitl-hitlSession-";
-export * as hitlHitlSession from "./hitl-hitlSession-";
-
-export const interfaces = {
-  "hitl<hitlSession>": hitlHitlSession.hitlHitlSession,
-}
--- integrations/chatwoot/bp_modules/hitl/definition/states/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/* tslint:disable */
-// This file is generated. Do not edit it manually.
-import * as hitlMessageHistory from "./hitlMessageHistory";
-export * as hitlMessageHistory from "./hitlMessageHistory";
-
-export const states = {
-  "hitlMessageHistory": hitlMessageHistory.hitlMessageHistory,
-}
