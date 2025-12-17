44218a670c22279be3379996b1cbdfa93f5d9ac5
Ceyhun <ceyhun.topcu@botpress.com>
2025-12-17 09:09:44 -0500

    fix: restore correct interface type for hitl bp_module

--- integrations/chatwoot/bp_modules/hitl/index.ts
+++ integrations/chatwoot/bp_modules/hitl/index.ts
@@ -7,10 +7,10 @@ import * as sdk from "@botpress/sdk"
 import definition from "./definition"
 
 export default {
-  type: "integration",
-  id: "intver_01K0SV5HZAT9FSVVEB7S91SH4F",
+  type: "interface",
+  id: "ifver_01K0STHY79Y0GFGFGDXXGQKDVE",
   uri: undefined,
   name: "hitl",
-  version: "2.0.2",
+  version: "2.0.0",
   definition,
-} satisfies sdk.IntegrationPackage
+} satisfies sdk.InterfacePackage
