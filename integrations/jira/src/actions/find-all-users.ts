a07889d95078dbfd8af9e7f4c1b515a8784addd7
Simon Risk <76711152+SimonNRisk@users.noreply.github.com>
2025-12-19 10:30:33 -0500

    fix: bump and format jira to pass ci (#231)
    * fix: skip jira in github setup action
    
    * fix: try to add botpress cli to dev dependencies
    
    * chore: bump patch version
    
    * rev: just skip integration
    
    * chore: prettier
    
    * rev: skipped integration

--- integrations/jira/src/actions/find-all-users.ts
+++ integrations/jira/src/actions/find-all-users.ts
@@ -1,18 +1,11 @@
 import { Version3Models } from 'jira.js'
 
-import {
-  findAllUsersInputSchema,
-  findAllUsersOutputSchema,
-} from '../misc/custom-schemas'
+import { findAllUsersInputSchema, findAllUsersOutputSchema } from '../misc/custom-schemas'
 import type { Implementation } from '../misc/types'
 
 import { getClient } from '../utils'
 
-export const findAllUsers: Implementation['actions']['findAllUsers'] = async ({
-  ctx,
-  input,
-  logger,
-}) => {
+export const findAllUsers: Implementation['actions']['findAllUsers'] = async ({ ctx, input, logger }) => {
   const validatedInput = findAllUsersInputSchema.parse(input)
   const jiraClient = getClient(ctx.configuration)
   const addParams = {
@@ -22,9 +15,7 @@ export const findAllUsers: Implementation['actions']['findAllUsers'] = async ({
   let response
   try {
     response = await jiraClient.findAllUser(addParams)
-    logger
-      .forBot()
-      .info(`Successful - Find All User - Total Users ${response.length}`)
+    logger.forBot().info(`Successful - Find All User - Total Users ${response.length}`)
   } catch (error) {
     logger.forBot().debug(`'Find All User' exception ${JSON.stringify(error)}`)
     response = [] as Version3Models.User[]
