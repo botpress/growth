ce452d50a530ae449e94dcc265d61ccbf3e15fca
ceyhun-botpress <ceyhun.topcu@botpress.com>
2025-12-22 10:36:20 -0500

    feat: Chatwoot Integration (#229)
    * feat: chatwoot hitl
    
    * feat: stick assignment for chatwoot tickets
    
    * feat: added image/file/mp4 support
    
    * feat: assign hitl to the ongoing conversation
    
    * chore: minor improvements
    
    * feat(chatwoot): enhance messaging capabilities and improve setup instructions
    
    * feat(chatwoot): update integration definition and improve contact handling
    
    * chore: change bp _modules in .gitignore
    
    * chore: revert gitignore
    
    * chore: remove tracked files
    
    * chore: remove pnpm lock
    
    * chore: add bp_modules
    
    * Revert "chore: add bp_modules"
    
    This reverts commit e00196911e7cf93555e9cf050b8e2e344a3ff553.
    
    * chore(chatwoot): update package.json to add build script and upgrade dependencies
    
    * refactor(setup-integrations): streamline integration setup process and update logging messages
    
    * refactor(setup-integrations): revert to previous
    
    * refactor(setup-integrations): revert back to original
    
    * feat(hitl): add HITL interface with message schemas and actions, update chatwoot integration build process
    
    * Revert "feat(hitl): add HITL interface with message schemas and actions, update chatwoot integration build process"
    
    This reverts commit 542b6aec537597d5381071abdc9c4324ab5e9d80.
    
    * fix(chatwoot): update hitl dependency path and enhance integration build process
    
    * feat(setup-integrations): enhance integration build process to conditionally run bp add for local bpDependencies
    
    * feat(setup-integrations): refine condition for running bp add based on local bpDependencies and SDK version
    
    * chore(dependencies): update @botpress/client, @botpress/sdk, and @botpress/cli versions in package.json for chatwoot and hitl integrations
    
    * refactor(hitl): simplify message schema and enhance message definitions with userId support
    
    * chore: revert everything, push bp_modules
    
    * chore: revert setup script
    
    * fix: restore correct interface type for hitl bp_module
    
    * chore(chatwoot): address pr comments
    
    * chore(chatwoot): update version to 1.0.2 and refine schemas for improved type handling
    
    * chore(chatwoot): update version to 1.0.3, refine message handling, and simplify account retrieval logic
    
    * chore(chatwoot): address pr comments
    
    * chore(chatwoot): update integration name and version

---
+++ integrations/chatwoot/bp_modules/hitl/definition/states/index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/* tslint:disable */
+// This file is generated. Do not edit it manually.
+import * as hitlMessageHistory from "./hitlMessageHistory";
+export * as hitlMessageHistory from "./hitlMessageHistory";
+
+export const states = {
+  "hitlMessageHistory": hitlMessageHistory.hitlMessageHistory,
+}
