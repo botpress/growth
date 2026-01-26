0d26738de9ff9cc5dad0cd115cebb90f8d0d123b
Eric Huang <eric.huang@botpress.com>
2026-01-26 11:18:41 -0500

    fix: Force commited bp modules

---
+++ integrations/zoho-sales-iq-hitl/bp_modules/hitl/definition/entities/hitlSession.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+// This file is generated. Do not edit it manually.
+
+import { z } from "@botpress/sdk";
+export const hitlSession = {
+  title: "HITL session",
+  description:
+    "A HITL session, often referred to as a ticket or conversation in external systems",
+  schema: z.object({}).catchall(z.never()),
+};
